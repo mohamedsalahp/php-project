72b5eae072c49f991c1f6bbe13b69b4ec2b302b5
mohamedsalahp <blazeonsim107@gmail.com>
2024-03-19 01:48:36 +0200

    change 404 error

--- includes/exception_error.php
+++ includes/exception_error.php
@@ -5,7 +5,7 @@
  */
  $GET_ROUTES = isset($routes['GET'])?$routes['GET']:[];
       if(!isset($_POST['_method']) && !is_null(segment()) && !in_array(segment(),array_column($GET_ROUTES,'segement'))){
-        //view('404');
+        view('404');
          exit();
       }
  
